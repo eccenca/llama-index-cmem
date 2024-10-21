14f57cf4461e9155be4e6f1ad130f855babe0446
Sebastian Tramp <sebastian.tramp@eccenca.com>
2024-10-21 12:26:01 +0200

    init

---
+++ tests/test_example.py
@@ -0,0 +1,11 @@
+"""Example tests.
+
+Remove this and other example files after bootstrapping your project.
+"""
+
+from llama_index_cmem.example import double
+
+
+def test_example() -> None:
+    """Example Test"""
+    assert double(10) == 10 * 2
