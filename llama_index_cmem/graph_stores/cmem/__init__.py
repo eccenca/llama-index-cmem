95d71ebf62d7fbe6a098eda8e8a6d2fe85b17904
Saipraneeth <saipraneeth.muddana@eccenca.com>
2025-01-17 07:22:54 +0000

    remove graph_stores module and update cmem_retriever

--- llama_index_cmem/graph_stores/cmem/__init__.py
+++
@@ -1,5 +0,0 @@
-"""llama-index-cmem.graph_stores.cmem"""
-
-from llama_index_cmem.graph_stores.cmem.cmem_graph_store import CMEMGraphStore
-
-__all__ = ["CMEMGraphStore"]
--- llama_index_cmem/retrievers/cmem/cmem_retriever.py
+++ llama_index_cmem/retrievers/cmem/cmem_retriever.py
@@ -1,18 +1,29 @@
 """CMEM retriever 2"""
 
 import logging
+from typing import Any
 
+from cmem.cmempy.queries import SparqlQuery
 from llama_index.core import QueryBundle, Settings
 from llama_index.core.base.base_retriever import BaseRetriever
-from llama_index.core.graph_stores.types import GraphStore
 from llama_index.core.llms import LLM
 from llama_index.core.schema import NodeWithScore, TextNode
 
-from llama_index_cmem.graph_stores.cmem.cmem_graph_store import is_empty_result
 from llama_index_cmem.utils.cmem_query_builder import CMEMQueryBuilder
 
 logger = logging.getLogger(__name__)
 DEFAULT_RETRIES = 5
+
+
+def is_empty_result(response: dict) -> bool:
+    """Check if a cmem response is empty."""
+    if response:
+        results = response.get("results")
+        if results:
+            bindings = results.get("bindings")
+            if bindings:
+                return len(bindings) < 1
+    return True
 
 
 class CMEMRetriever(BaseRetriever):
@@ -20,13 +31,11 @@ class CMEMRetriever(BaseRetriever):
 
     def __init__(
         self,
-        graph_store: GraphStore,
         ontology_graph: str,
         context_graph: str,
         llm: LLM,
     ) -> None:
         super().__init__()
-        self.graph_store = graph_store
         self.query_builder = CMEMQueryBuilder(
             ontology_graph=ontology_graph, context_graph=context_graph, llm=llm
         )
@@ -35,7 +44,7 @@ class CMEMRetriever(BaseRetriever):
 
     def _retrieve(self, query_bundle: QueryBundle) -> list[NodeWithScore]:
         cmem_query = self.query_builder.generate_sparql(question=query_bundle.query_str)
-        response = self.graph_store.query(query=cmem_query.get_last_sparql())
+        response = self._query(query=cmem_query.get_last_sparql())
         index = 0
         while is_empty_result(response) and index < DEFAULT_RETRIES:
             logger.info("Empty CMEM query. Try another query.")
@@ -47,3 +56,13 @@ class CMEMRetriever(BaseRetriever):
         metadata = {"cmem_query": cmem_query, "cmem_response": response}
         node = TextNode(text=str(response), metadata=metadata)
         return [NodeWithScore(node=node, score=1.0)]
+
+    def _query(self, query: str, param_map: dict[str, Any] | None = None) -> object:
+        """Query CMEM graph store"""
+        placeholder = None
+        if param_map:
+            placeholder = param_map["placeholder"]
+        logger.info(f"SPARQL Query: {0}".format(query))
+        response = SparqlQuery(query, placeholder=placeholder).get_json_results()
+        logger.info(f"CMEM Response: {response!s}")
+        return response
