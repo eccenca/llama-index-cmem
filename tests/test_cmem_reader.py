86aadc30cf7838f951b51680d6a8c64bec7a484e
Peter Freytag <peter.freytag@eccenca.com>
2025-01-24 14:41:26 +0100

    add test annotations

--- tests/test_cmem_reader.py
+++ tests/test_cmem_reader.py
@@ -1,10 +1,13 @@
 """Testing cmem reader"""
+import pytest
 
 from llama_index_cmem.readers.cmem import CMEMReader
+from tests.utils import needs_cmem
 
 NUMBER_OF_DOCUMENTS = 20043
 
-
+@needs_cmem
+@pytest.mark.usefixtures("graph_setup")
 def test_cmem_reader() -> None:
     """Testing cmem reader"""
     cmem_reader = CMEMReader()
