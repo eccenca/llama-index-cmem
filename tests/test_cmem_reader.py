84f8814b13df40882d9fb19be0d44b4706217b37
Peter Freytag <peter.freytag@eccenca.com>
2025-01-24 22:20:57 +0100

    fix graph name

--- tests/test_cmem_reader.py
+++ tests/test_cmem_reader.py
@@ -13,6 +13,6 @@ NUMBER_OF_DOCUMENTS = 20043
 def test_cmem_reader() -> None:
     """Testing cmem reader"""
     cmem_reader = CMEMReader()
-    graph = "<http://ld.company.org/prod-inst/>"
+    graph = "http://ld.company.org/prod-inst/"
     documents = cmem_reader.load_data(graph)
     assert len(documents) == NUMBER_OF_DOCUMENTS
