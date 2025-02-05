f2e36e3a87330565dbc903f6ce425f8bd652a5bb
Peter Freytag <peter.freytag@eccenca.com>
2025-02-05 16:23:40 +0100

    add catalog retriever

---
+++ llama_index_cmem/retrievers/cmem/base.py
@@ -0,0 +1,13 @@
+"""llama_index retrievers cmem base"""
+
+from llama_index.core.schema import NodeWithScore, TextNode
+
+
+def auto_convert_results(
+    results: dict, metadata: dict = None, score: float = 1.0
+) -> list[NodeWithScore]:
+    """Convert a results dictionary to a list of NodeWithScore objects"""
+    if metadata is None:
+        metadata = {}
+    node = TextNode(text=str(results), metadata=metadata)
+    return [NodeWithScore(node=node, score=score)]
---
+++ llama_index_cmem/retrievers/cmem/catlog.py
@@ -0,0 +1,30 @@
+"""Catalog retriever."""
+
+from cmem.cmempy.queries import QueryCatalog
+from llama_index.core import QueryBundle, Settings
+from llama_index.core.base.base_retriever import BaseRetriever
+from llama_index.core.llms import LLM
+from llama_index.core.schema import NodeWithScore
+
+from llama_index_cmem.retrievers.cmem.base import auto_convert_results
+
+
+class CatalogRetriever(BaseRetriever):
+    """Catalog Retriever."""
+
+    def __init__(
+        self,
+        identifier: str,
+        placeholder: dict = None,
+        llm: LLM = None,
+    ) -> None:
+        super().__init__()
+        self.identifier = identifier
+        self.placeholder = placeholder
+        self.llm = llm or Settings.llm
+
+    def _retrieve(self, query_bundle: QueryBundle) -> list[NodeWithScore]:
+        catalog = QueryCatalog()
+        query = catalog.get_query(self.identifier, self.placeholder)
+        results = query.get_json_results(placeholder=self.placeholder)
+        return auto_convert_results(results)
