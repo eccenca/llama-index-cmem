50435eeb91b6cf2ca0f55f8eefc83a8088cb06d2
saipraneeth <2506664+msaipraneeth@users.noreply.github.com>
2025-03-17 07:25:00 +0000

    fix test_catalog_retriever

--- tests/test_catalog_retriever.py
+++ tests/test_catalog_retriever.py
@@ -8,7 +8,10 @@ from llama_index.core import QueryBundle, Settings
 from llama_index.core.schema import NodeWithScore
 from llama_index.embeddings.openai import OpenAIEmbedding
 
-from llama_index_cmem.retrievers.cmem.catlog import CatalogRetriever
+from llama_index_cmem.retrievers.cmem.catlog import (
+    CatalogAutoSelectRetriever,
+    CatalogVectorRetriever,
+)
 
 CATALOG_RETRIEVER_PROPERTIES_BINDINGS = 33
 
@@ -39,16 +42,16 @@ def extract_metadata(nodes_with_score: list[NodeWithScore]) -> dict[str, Any]:
 @pytest.mark.usefixtures("graph_setup")
 def test_catalog_retriever_unknown_query() -> None:
     """Test catalog retriever with an unknown query"""
-    catalog_retriever = CatalogRetriever(identifier=":unknown-query")
-    retrieved_nodes = catalog_retriever.retrieve(QueryBundle(query_str="n/a"))
+    catalog_retriever = CatalogAutoSelectRetriever()
+    retrieved_nodes = catalog_retriever.retrieve(QueryBundle(query_str=":unknown-query"))
     assert len(retrieved_nodes) == 0
 
 
 @pytest.mark.usefixtures("graph_setup")
 def test_catalog_retriever_services() -> None:
     """Test catalog retriever without placeholders and services"""
-    catalog_retriever = CatalogRetriever(identifier=":all-services")
-    retrieved_nodes = catalog_retriever.retrieve(QueryBundle(query_str="n/a"))
+    catalog_retriever = CatalogAutoSelectRetriever()
+    retrieved_nodes = catalog_retriever.retrieve(QueryBundle(query_str=":all-services"))
     assert len(retrieved_nodes) == 1
     bindings = extract_bindings(retrieved_nodes)
     assert len(bindings) == CATALOG_RETRIEVER_SERVICES_BINDINGS
@@ -57,8 +60,8 @@ def test_catalog_retriever_services() -> None:
 @pytest.mark.usefixtures("graph_setup")
 def test_catalog_retriever_hardware() -> None:
     """Test catalog retriever without placeholders and hardware"""
-    catalog_retriever = CatalogRetriever(identifier=":all-hardware")
-    retrieved_nodes = catalog_retriever.retrieve(QueryBundle(query_str="n/a"))
+    catalog_retriever = CatalogAutoSelectRetriever()
+    retrieved_nodes = catalog_retriever.retrieve(QueryBundle(query_str=":all-hardware"))
     assert len(retrieved_nodes) == 1
     bindings = extract_bindings(retrieved_nodes)
     assert len(bindings) == CATALOG_RETRIEVER_HARDWARE_BINDINGS
@@ -67,9 +70,10 @@ def test_catalog_retriever_hardware() -> None:
 @pytest.mark.usefixtures("graph_setup")
 def test_catalog_retriever_with_placeholder_classes() -> None:
     """Test catalog retriever with placeholders and classes"""
-    placeholder = {"graph": "http://ld.company.org/prod-inst/"}
-    catalog_retriever = CatalogRetriever(identifier=":list-classes", placeholder=placeholder)
-    retrieved_nodes = catalog_retriever.retrieve(QueryBundle(query_str="n/a"))
+    catalog_retriever = CatalogAutoSelectRetriever()
+    retrieved_nodes = catalog_retriever.retrieve(
+        QueryBundle(query_str=":list-classes for graph http://ld.company.org/prod-inst/")
+    )
     assert len(retrieved_nodes) == 1
     bindings = extract_bindings(retrieved_nodes)
     assert len(bindings) == CATALOG_RETRIEVER_CLASSES_BINDINGS
@@ -78,9 +82,10 @@ def test_catalog_retriever_with_placeholder_classes() -> None:
 @pytest.mark.usefixtures("graph_setup")
 def test_catalog_retriever_with_placeholder_properties() -> None:
     """Test catalog retriever with placeholders and properties"""
-    placeholder = {"graph": "http://ld.company.org/prod-inst/"}
-    catalog_retriever = CatalogRetriever(identifier=":list-properties", placeholder=placeholder)
-    retrieved_nodes = catalog_retriever.retrieve(QueryBundle(query_str="n/a"))
+    catalog_retriever = CatalogAutoSelectRetriever()
+    retrieved_nodes = catalog_retriever.retrieve(
+        QueryBundle(query_str=":list-properties for graph http://ld.company.org/prod-inst/")
+    )
     assert len(retrieved_nodes) == 1
     bindings = extract_bindings(retrieved_nodes)
     assert len(bindings) == CATALOG_RETRIEVER_PROPERTIES_BINDINGS
@@ -90,7 +95,7 @@ def test_catalog_retriever_with_placeholder_properties() -> None:
 def test_catalog_retriever_vector_retrieve_services() -> None:
     """Test catalog retriever using vectorstore retrieve"""
     Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
-    catalog_retriever = CatalogRetriever(use_vector_retrieve=True)
+    catalog_retriever = CatalogVectorRetriever()
     retrieved_nodes = catalog_retriever.retrieve(
         QueryBundle(query_str="Show me all service from product demo.")
     )
