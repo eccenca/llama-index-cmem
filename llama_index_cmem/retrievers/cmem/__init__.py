1463cd8cb54787d5c47967317aceebdd4fabb239
saipraneeth <2506664+msaipraneeth@users.noreply.github.com>
2025-03-17 11:19:40 +0000

    adapt jupyter notebook

--- llama_index_cmem/retrievers/cmem/__init__.py
+++ llama_index_cmem/retrievers/cmem/__init__.py
@@ -1,6 +1,6 @@
 """llama-index-cmem.retrievers.cmem"""
 
-from llama_index_cmem.retrievers.cmem.catlog import CatalogAutoSelectRetriever
+from llama_index_cmem.retrievers.cmem.catlog import CatalogAutoSelectRetriever, CatalogVectorRetriever
 from llama_index_cmem.retrievers.cmem.cmem_retriever import CMEMRetriever
 
-__all__ = ["CMEMRetriever", "CatalogAutoSelectRetriever"]
+__all__ = ["CMEMRetriever", "CatalogAutoSelectRetriever", "CatalogVectorRetriever"]
